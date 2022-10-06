c3724333371741a1fe023085310845f9f728d487
Marina <98214485+Marina-chepchugova@users.noreply.github.com>
2022-10-06 15:51:38 +1100

    added keypress event

--- starter/script.js
+++ starter/script.js
@@ -12,9 +12,17 @@ const openModal = function()	{
 	modal.classList.remove('hidden');
 	overlay.classList.remove('hidden');
 }
-for(let i = 0; i < btsOpenModal.length; i++)	
-btsOpenModal[i].addEventListener('click', openModal)
+for(let i = 0; i < btsOpenModal.length; i++)
+btsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown' , function(e)	{
+	if(e.key === 'Escape'	&&		!modal.classList.contains('hidden'))	{
+			closeModal();
+		}
+	
+	}
+
+);
 		
